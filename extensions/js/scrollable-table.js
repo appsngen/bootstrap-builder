ec2502a47012a2ab05b6445cfde36e16816dbe97
Yuliya Filipava <Yuliya_Filipava1@epam.com>
2015-05-15 12:41:53 +0300

    update default options in scrollable table constructor

--- extensions/js/scrollable-table.js
+++ extensions/js/scrollable-table.js
@@ -46,8 +46,8 @@
         this.version = '0.0.7';
         this.settings = {
             mouseWheelSpeed: 30,
-            horizontalGutter: 1,
-            verticalGutter: 1
+            horizontalGutter: -8,
+            verticalGutter: -8
         };
         $.extend(this.settings, jspOptionsObj);
 
