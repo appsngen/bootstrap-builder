19728db591d25b3810eb84585e5a1a9dd9098f28
Yuliya Filipava <Yuliya_Filipava1@epam.com>
2015-05-21 14:43:12 +0300

    edit default options in constructor

--- extensions/js/scrollable-container.js
+++ extensions/js/scrollable-container.js
@@ -20,8 +20,8 @@
         this.version = '0.0.1';
         this.settings = {
             mouseWheelSpeed: 30,
-            horizontalGutter: -8,
-            verticalGutter: -8
+            horizontalGutter: 1,
+            verticalGutter: 1
         };
         $.extend(this.settings, jspOptionsObj);
 
